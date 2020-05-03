0e531a5e2f7f3eaefc368f50698d586d1945837d
Fabiano Amaral <fabianoamaral445@gmail.com>
2020-05-03 17:36:22 -0300

    adicionando scripts e estilos

---
+++ script/script.js
@@ -0,0 +1,22 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+
+let snake = [];
+snake[0] ={
+    x:8 * box,
+    y:8*box
+}
+
+function createBackground()
+{
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16*box, 16*box);
+}
+
+function createSnake()
+{
+
+}
+
+createBackground();
