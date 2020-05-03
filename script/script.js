93da88a7cdb95a34e448e03a666c9187fceef9f3
Fabiano Amaral <fabianoamaral445@gmail.com>
2020-05-03 20:24:51 -0300

    adicionando movimento a snake

--- script/script.js
+++ script/script.js
@@ -3,10 +3,12 @@ let context = canvas.getContext("2d");
 let box = 32;
 
 let snake = [];
-snake[0] ={
-    x:8 * box,
-    y:8*box
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
 }
+
+let direction = "right";
 
 function createBackground()
 {
@@ -16,7 +18,50 @@ function createBackground()
 
 function createSnake()
 {
-
+    for(let i=0; i < snake.length; i++)
+    {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
 
-createBackground();
+function checkDirection(direction, snakex, snakey)
+{
+    switch(direction)
+    {
+        case "right":
+            snake += box;
+            break;
+        case "left":
+            snakex -= box;
+            break;
+        case "down":
+            snakey += box;
+            break;
+        case "up":
+            snakey -= box;
+            break;
+    }
+}
+
+function initGame() // Function definida para iniciar o jogo
+{   
+        createBackground();
+        createSnake();
+
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
+
+        checkDirection(direction, snakeX, snakeY);
+        snake.pop();
+
+        let newHead = {
+            x: snakeX,
+            y: snakeY
+        }
+
+        snake.unshift(newHead);
+}
+
+let game = setInterval(initGame, 100);
+
